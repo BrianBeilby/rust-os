ccdbc842f9d8edb4545f3c06987ed7cd44b02565
BrianBeilby <brianbeilby98@gmail.com>
2024-06-26 16:20:46 -0700

    Start memory mapping

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,13 @@
 #![test_runner(rust_os::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
+use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use rust_os::println;
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
+entry_point!(kernel_main);
+
+fn kernel_main(boot_info: &'static BootInfo) -> ! {
     println!("Hello World{}", "!");
 
     rust_os::init();
