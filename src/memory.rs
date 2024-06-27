200526bff72256bffcaee7e8089eefc61ab1a20d
BrianBeilby <brianbeilby98@gmail.com>
2024-06-27 15:14:23 -0700

    Update memory.rs

--- src/memory.rs
+++ src/memory.rs
@@ -37,3 +37,11 @@ pub fn create_example_mapping(
     let map_to_result = unsafe { mapper.map_to(page, frame, flags, frame_allocator) };
     map_to_result.expect("map_to failed").flush();
 }
+
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        None
+    }
+}
