dc16cd1eeedfa31c81014b2a47a9d07bada2f4b5
BrianBeilby <brianbeilby98@gmail.com>
2024-07-01 16:26:42 -0700

    Create mod.rs

---
+++ src/task/mod.rs
@@ -0,0 +1,14 @@
+use alloc::boxed::Box;
+use core::{future::Future, pin::Pin};
+
+pub struct Task {
+    future: Pin<Box<dyn Future<Output = ()>>>,
+}
+
+impl Task {
+    pub fn new(future: impl Future<Output = ()> + 'static) -> Task {
+        Task {
+            future: Box::pin(future),
+        }
+    }
+}
