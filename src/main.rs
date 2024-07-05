431c84f559c60d75d209f4969433ff93bd9fd53f
BrianBeilby <brianbeilby98@gmail.com>
2024-07-04 20:09:59 -0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,11 @@
 
 extern crate alloc;
 
-use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 use bootloader::{entry_point, BootInfo};
-use core::{num, panic::PanicInfo};
+use core::panic::PanicInfo;
 use rust_os::println;
-use rust_os::task::{simple_executor::SimpleExecutor, Task};
+use rust_os::task::simple_executor::SimpleExecutor;
+use rust_os::task::{keyboard, Task};
 
 entry_point!(kernel_main);
 
@@ -30,6 +30,7 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
 
     let mut executor = SimpleExecutor::new();
     executor.spawn(Task::new(example_task()));
+    executor.spawn(Task::new(keyboard::print_keypresses())); // new
     executor.run();
 
     #[cfg(test)]
